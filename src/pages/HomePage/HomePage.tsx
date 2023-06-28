f15b02b5ab1ba5b68ab10a170213c24038e6ad62
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-06-28 19:21:05 +0200

    Corregidos header container y footer

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,16 +6,20 @@ import "./HomePage.scss";
 const HomePage = (): JSX.Element => {
   return (
     <div className="home-page page">
-      <Box boxShadow="md" p="6" rounded="md" bg="w">
+      <Box className="home-page__header" boxShadow="md" p="6" rounded="md" bg="w">
         <Header></Header>
       </Box>
-      <p className="home-page__title">
-        Evalue la<br></br>madurez cultural de su empresa
-      </p>
-      <Button size="sm" colorScheme="blue" variant="solid">
-        Comenzar
-      </Button>
-      <Footer></Footer>
+      <div className="home-page__container">
+        <p className="home-page__title">
+          Evalue la<br></br>madurez cultural de su empresa
+        </p>
+        <Button size="sm" colorScheme="blue" variant="solid">
+          Comenzar
+        </Button>
+      </div>
+      <Box className="home-page__footer" boxShadow="md" p="6" rounded="md" bg="w">
+        <Footer></Footer>
+      </Box>
     </div>
   );
 };
