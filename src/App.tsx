4bc10dc832a3acf47e90a74a9ed2f998d9a89c2e
Karl <karlbrandon9@gmail.com>
2023-06-29 12:34:04 +0200

    Added text page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
 import FormPage from "./pages/FormPage/FormPage";
 import { ChakraProvider } from "@chakra-ui/react";
+import FormPageText from "./pages/FormPageText/FormPageText";
 
 function App(): JSX.Element {
   return (
@@ -12,6 +13,7 @@ function App(): JSX.Element {
           <Routes>
             <Route path="/" element={<HomePage></HomePage>}></Route>
             <Route path="/questionary" element={<FormPage></FormPage>}></Route>
+            <Route path="/text-questionary" element={<FormPageText></FormPageText>}></Route>
           </Routes>
         </HashRouter>
       </ChakraProvider>
