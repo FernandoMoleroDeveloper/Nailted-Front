8882f4be46010057aae901bbb1d7c2c2e3eda57c
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-06-29 17:06:50 +0200

    Correcion estilo results

--- src/pages/ResultsPage/ResultsPage.tsx
+++ src/pages/ResultsPage/ResultsPage.tsx
@@ -13,37 +13,37 @@ const ResultsPage = (): JSX.Element => {
       <Progress value={20} size="xs" colorScheme="#0469da" />
       <Box className="results-page__container">
         <FormControl as="fieldset">
-          <FormLabel textAlign="center" as="legend">
+          <FormLabel margin="0px auto" textAlign="center" as="legend">
             Resultado general
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={49.2} color="orange.400" size="150px" thickness="10px">
             <CircularProgressLabel>49,2%</CircularProgressLabel>
           </CircularProgress>
-          <FormLabel textAlign="center" as="legend">
+          <FormLabel margin="0px auto" textAlign="center" as="legend">
             Direction
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={62} color="green.400" size="80px" thickness="8px">
             <CircularProgressLabel>62%</CircularProgressLabel>
           </CircularProgress>
-          <FormLabel textAlign="center" as="legend">
+          <FormLabel margin="0px auto" textAlign="center" as="legend">
             Feedback
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={48} color="orange.400" size="80px" thickness="8px">
             <CircularProgressLabel>48%</CircularProgressLabel>
           </CircularProgress>
-          <FormLabel textAlign="center" as="legend">
+          <FormLabel margin="0px auto" textAlign="center" as="legend">
             Recognition
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={36} color="orange.400" size="80px" thickness="8px">
             <CircularProgressLabel>36%</CircularProgressLabel>
           </CircularProgress>
-          <FormLabel textAlign="center" as="legend">
+          <FormLabel margin="0px auto" textAlign="center" as="legend">
             Wellness
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={22} color="red.400" size="80px" thickness="8px">
             <CircularProgressLabel>22%</CircularProgressLabel>
           </CircularProgress>
-          <FormLabel textAlign="center" as="legend">
+          <FormLabel margin="0px auto" textAlign="center" as="legend">
             Career
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={78} color="green.400" size="80px" thickness="8px">
