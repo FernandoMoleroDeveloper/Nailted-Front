40cf6aebddc911ee805cd402f94ed6fb5704db54
abj1389 <abj_13@hotmail.com>
2023-06-29 23:48:32 +0200

    improve results

--- src/pages/ResultsPage/ResultsPage.tsx
+++ src/pages/ResultsPage/ResultsPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, CircularProgress, CircularProgressLabel, FormControl, FormHelperText, FormLabel, Progress } from "@chakra-ui/react";
+import { Box, Button, CircularProgress, CircularProgressLabel, Divider, FormControl, FormHelperText, FormLabel, Progress } from "@chakra-ui/react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./ResultsPage.scss";
@@ -13,42 +13,70 @@ const ResultsPage = (): JSX.Element => {
       <Progress value={20} size="xs" colorScheme="#0469da" />
       <Box className="results-page__container">
         <FormControl as="fieldset">
-          <FormLabel margin="0px auto" textAlign="center" as="legend">
+          <FormLabel fontSize="24px" textColor="#199bf6" fontWeight="extrabold" margin="0px auto" textAlign="center" as="legend">
             Resultado general
           </FormLabel>
           <CircularProgress className="results-page__progress-circle" value={49.2} color="orange.400" size="150px" thickness="10px">
             <CircularProgressLabel>49,2%</CircularProgressLabel>
           </CircularProgress>
-          <FormLabel margin="0px auto" textAlign="center" as="legend">
-            Direction
-          </FormLabel>
-          <CircularProgress className="results-page__progress-circle" value={62} color="green.400" size="80px" thickness="8px">
-            <CircularProgressLabel>62%</CircularProgressLabel>
-          </CircularProgress>
-          <FormLabel margin="0px auto" textAlign="center" as="legend">
-            Feedback
-          </FormLabel>
-          <CircularProgress className="results-page__progress-circle" value={48} color="orange.400" size="80px" thickness="8px">
-            <CircularProgressLabel>48%</CircularProgressLabel>
-          </CircularProgress>
-          <FormLabel margin="0px auto" textAlign="center" as="legend">
-            Recognition
-          </FormLabel>
-          <CircularProgress className="results-page__progress-circle" value={36} color="orange.400" size="80px" thickness="8px">
-            <CircularProgressLabel>36%</CircularProgressLabel>
-          </CircularProgress>
-          <FormLabel margin="0px auto" textAlign="center" as="legend">
-            Wellness
-          </FormLabel>
-          <CircularProgress className="results-page__progress-circle" value={22} color="red.400" size="80px" thickness="8px">
-            <CircularProgressLabel>22%</CircularProgressLabel>
-          </CircularProgress>
-          <FormLabel margin="0px auto" textAlign="center" as="legend">
-            Career
-          </FormLabel>
-          <CircularProgress className="results-page__progress-circle" value={78} color="green.400" size="80px" thickness="8px">
-            <CircularProgressLabel>78%</CircularProgressLabel>
-          </CircularProgress>
+          <Divider className="results-page__horizontal-divider" />
+          <Box display="flex" flexWrap="wrap">
+            <Box width="40%" display="flex" alignItems="center">
+              <CircularProgress className="results-page__progress-circle" value={62} color="green.400" size="65px" thickness="8px">
+                <CircularProgressLabel>62%</CircularProgressLabel>
+              </CircularProgress>
+              <Box display="flex" flexWrap="wrap" margin="0 0 0 5px">
+                <FormLabel fontWeight="extrabold" margin="0px" textAlign="left" as="legend">
+                  Direction
+                </FormLabel>
+                <FormLabel>Prueba</FormLabel>
+              </Box>
+            </Box>
+            <Box width="40%" display="flex" alignItems="center">
+              <CircularProgress className="results-page__progress-circle" value={48} color="orange.400" size="65px" thickness="8px">
+                <CircularProgressLabel>48%</CircularProgressLabel>
+              </CircularProgress>
+              <Box display="flex" flexWrap="wrap" margin="0 0 0 5px">
+                <FormLabel fontWeight="extrabold" margin="0px" textAlign="left" as="legend">
+                  Feedback
+                </FormLabel>
+                <FormLabel>Prueba</FormLabel>
+              </Box>
+            </Box>
+            <Box width="40%" display="flex" alignItems="center">
+              <CircularProgress className="results-page__progress-circle" value={36} color="orange.400" size="65px" thickness="8px">
+                <CircularProgressLabel>36%</CircularProgressLabel>
+              </CircularProgress>
+              <Box display="flex" flexWrap="wrap" margin="0 0 0 5px">
+                <FormLabel fontWeight="extrabold" margin="0px" textAlign="left" as="legend">
+                  Recognition
+                </FormLabel>
+                <FormLabel>Prueba</FormLabel>
+              </Box>
+            </Box>
+            <Box width="40%" display="flex" alignItems="center">
+              <CircularProgress className="results-page__progress-circle" value={22} color="red.400" size="65px" thickness="8px">
+                <CircularProgressLabel>22%</CircularProgressLabel>
+              </CircularProgress>
+              <Box display="flex" flexWrap="wrap" margin="0 0 0 5px">
+                <FormLabel fontWeight="extrabold" margin="0px" textAlign="left" as="legend">
+                  Wellness
+                </FormLabel>
+                <FormLabel>Prueba</FormLabel>
+              </Box>
+            </Box>
+            <Box width="40%" display="flex" alignItems="center">
+              <CircularProgress className="results-page__progress-circle" value={78} color="green.400" size="65px" thickness="8px">
+                <CircularProgressLabel>78%</CircularProgressLabel>
+              </CircularProgress>
+              <Box display="flex" flexWrap="wrap" margin="0 0 0 5px">
+                <FormLabel fontWeight="extrabold" margin="0px" textAlign="left" as="legend">
+                  Career
+                </FormLabel>
+                <FormLabel>Prueba</FormLabel>
+              </Box>
+            </Box>
+          </Box>
           <FormHelperText>Be better, my friend!</FormHelperText>
           <Button {...blueButton} className="results-page__button center">
             Siguiente
