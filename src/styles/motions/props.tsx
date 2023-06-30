cdc161e22ecea6343f16dbcaefcb8a157b1f3257
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-07-01 01:19:49 +0200

    Fixes boxes

--- src/styles/motions/props.tsx
+++ src/styles/motions/props.tsx
@@ -25,47 +25,21 @@ export const blueButton = {
 export const selectionBoxesButton = {
   border: "2px solid #199bf6",
   backgroundColor: "#fff",
-  _hover: { bg: "#199bf6", color: "#fff" },
+  transition: "background-color 0.3s ease, color 0.3s ease",
+  _active: { bg: "#199bf6", color: "#fff", transition: "background-color 0.3s ease, color 0.3s ease" },
   color: "#199bf6",
   borderRadius: "30px",
-  // _active: {
-  //   bg: "#0469da",
-  //   transform: "scale(0.98)",
-  //   borderColor: "#0469da",
-  // },
-  // _focus: {
-  //   bg: "#0469da",
-  //   transform: "scale(0.98)",
-  //   borderColor: "#0469da",
-  //   color: "#fff"
-  // }
 };
 
 export const selectionBoxesButtonActive = {
   border: "2px solid #199bf6",
   backgroundColor: "#199bf6",
-  _hover: { bg: "#199bf6", color: "#fff" },
+  transition: "background-color 0.3s ease, color 0.3s ease",
+  _active: { bg: "#199bf6", color: "#fff", transition: "background-color 0.3s ease, color 0.3s ease" },
   transform: "scale(0.98)",
   color: "#fff",
   borderRadius: "30px"
 };
-
-// export const selectionBoxesButtonActive = {
-//   as: motion.div,
-//   transition: "background-color 0.3s",
-//   border: "3px solid transparent",
-//   borderRadius: "15px",
-//   bg: "#179bf6",
-//   color: "#ffff",
-//   _hover: { bg: "#0069D9" },
-//   _active: {
-//     boxShadow: "0 0 0 3px #179bf6",
-//     transition: "box-shadow 0.2s",
-//   },
-//   _focus: {
-//     boxShadow: "0 0 0 3px #179bf6",
-//   },
-// }
 
 export const nextButton = {
   as: motion.div,
