214586f7008a8fdbfb75ee22b12b0ef4016b6aeb
Fernando <fmoleromartin@gmail.com>
2023-07-17 02:37:12 +0200

    email modal

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,8 +1,11 @@
-import { Box, Text, CircularProgress, CircularProgressLabel, Divider } from "@chakra-ui/react";
+import { Box, Text, CircularProgress, CircularProgressLabel, Divider, Button, Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, ModalBody, FormControl, FormLabel, Input, ModalFooter, useDisclosure, Switch, Link, Flex } from "@chakra-ui/react";
 import "../../styles/layouts/ResultsPage.scss";
 import ResultsCategory from "./ResultsCategory";
+import { useRef } from "react";
 
 const Results = (): JSX.Element => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = useRef(null);
   return (
     <div className="results-page">
       <Box className="results-page__container">
@@ -20,6 +23,33 @@ const Results = (): JSX.Element => {
           <ResultsCategory></ResultsCategory>
           <ResultsCategory></ResultsCategory>
         </Box>
+        <Button marginTop="20px" onClick={onOpen}>
+          Enviar mis resultados
+        </Button>
+        <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>¿Quieres recibir tus resultados?</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Escribe aquí tu e-mail:</FormLabel>
+                <Input ref={initialRef} placeholder="ejemplo@mail.com" />
+              </FormControl>
+            </ModalBody>
+            <Flex alignItems="center" margin="0px auto">
+              <Switch colorScheme="teal" size="md" />
+              <Link marginLeft="10px" href="https://nailted.com/es/legal/privacy" isExternal>
+                Política de privacidad
+              </Link>
+            </Flex>
+            <ModalFooter>
+              <Button colorScheme="blue" onClick={onClose}>
+                Enviar
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
     </div>
   );
