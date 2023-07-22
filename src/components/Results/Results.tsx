4fa07a99fadfd0c38bb0d3f0cf053dcce58ea3bb
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-07-23 01:31:20 +0200

    Correcciones en Results con token

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -24,7 +24,7 @@ const Results = (): React.JSX.Element => {
   const location = useLocation();
   const queryParams = QueryString.parse(location.search, { ignoreQueryPrefix: true });
   const localOrPropOrParamSessionId: string = sessionId || queryParams.id || localStorage.getItem("storedSessionId");
-  const localOrPropOrParamToken: string = token || queryParams.owner || localStorage.getItem("storedToken");
+  const [localOrPropOrParamToken, setLocalOrPropOrParamToken] = useState<string>(token || queryParams.owner || localStorage.getItem("storedToken"));
   const SEND_EMAIL_URL = `${process.env.REACT_APP_API_URL as string}/session/${localOrPropOrParamSessionId}/send-results`;
   const SESSION_URL = `${process.env.REACT_APP_API_URL as string}/session/${localOrPropOrParamSessionId}/results`;
 
@@ -48,12 +48,12 @@ const Results = (): React.JSX.Element => {
         },
         body: JSON.stringify({ email, dataResults: results }),
       })
-        .then((res) => {
+        .then(async (res) => {
           if (res.status !== 200) {
             console.error("La respuesta del servidor no fue la esperada. El correo no se ha enviado.");
           }
-          console.log("Respuesta despues de enviar el email.");
-          console.log(res.json());
+          const data = await res.json();
+          setLocalOrPropOrParamToken(data.owner);
           setEmailSent(true);
           setEmail("");
         })
@@ -83,8 +83,6 @@ const Results = (): React.JSX.Element => {
   };
 
   const getResults = async (): Promise<void> => {
-    console.log(SESSION_URL);
-    console.log("El token que le mando es: ", localOrPropOrParamToken)
     fetch(SESSION_URL, {
       method: "PUT",
       headers: {
@@ -94,12 +92,9 @@ const Results = (): React.JSX.Element => {
     })
       .then(async (res) => {
         if (res.status !== 200) {
-          console.error("La respuesta del servidor no fue la esperada. Los resultados no se han cargado.");
         } else {
           const data = await res.json();
           setResults(data);
-          console.log("Resultados cargados correctamente.");
-          console.log(data);
         }
       })
       .catch((error) => {
@@ -108,24 +103,19 @@ const Results = (): React.JSX.Element => {
   };
 
   useEffect(() => {
-    console.log("ID: ", localOrPropOrParamSessionId);
-    console.log("Token: ", localOrPropOrParamToken);
     if (localOrPropOrParamSessionId && localOrPropOrParamToken) {
-      console.log("Voy a por los resultados");
       void getResults();
     }
     if (emailSent) emailSent && onClose(); // sobra el if?
     if (queryParams.id) {
       localStorage.setItem("storedSessionId", queryParams.id as string);
       localStorage.setItem("storedToken", queryParams.token as string);
-      console.log(localStorage.getItem("storedSessionId"));
-      console.log(localStorage.getItem("storedToken"));
       navigate("/results");
     }
     if (!localOrPropOrParamSessionId) {
       navigate("/");
     }
-  }, [results, emailSent, localOrPropOrParamSessionId]);
+  }, [emailSent, localOrPropOrParamSessionId]);
 
   return (
     <div className="results-page">
